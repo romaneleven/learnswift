989a992cc1f7dbd5edc6165ace83de321927b3b3
Xi Guo <xiguo@me.com>
2017-01-28 13:31:51 -0500

    start

--- Learn2/ViewController.swift
+++ Learn2/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var thelabel: UILabel!
+    var tapCount = 0
+    @IBAction func button_submit(_ sender: Any) {
+        
+        thelabel.text="Customer Info"
+        print("Form Submitted")
+        tapCount = tapCount+1
+        if tapCount>=10{
+            thelabel.text="you tapped submit 10 times or more"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
